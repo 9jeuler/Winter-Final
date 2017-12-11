1b7c8112c93b3b1596da366a3f6cb0e1f52cc58c
9jeuler <31480902+9jeuler@users.noreply.github.com>
2017-12-11 09:19:01 -0600

    Add files via upload

--- ChargingDock.js
+++ ChargingDock.js
@@ -1,28 +1,40 @@
-//defines how all charging stations work.
 function ChargingDock(){
 
-//Instance variables
-    this.ports; //finish from instructions
-    this.leds; //finish from instructions
+    this.ports = [undefined, undefined, undefined,undefined,undefined,undefined,undefined,undefined] //finish from instructions
+    this.leds = ["red","red","red","red","red","red","red","red",]
 
-
-//Instance Fucntions
     this.plug = function(dvc){
-        //type in here
+        for(let s = 0; s < this.ports.length; s++){
+            if (this.leds[s] == "red"){
+                this.port[s] = dvc;
+                this.leds[s] = "yellow";
+                break
+            }
+        }
     };
-  
+
     this.unplug = function(dvcIdx){
-        //type in here
+        if(this.leds[dvcIdx]  == "yellow" || this.leds[dvcIdx] == green){
+            let temp = this.ports[dvcIdx];
+            this.ports[dvcIdx] = undefined
+            this.leds[dvcIdx] = "red"
+            return temp
+        }
     };
-  
+
     this.chargeAll = function(min){
-        //type in here
+        for(let s = 0; s < this.ports.length; s++){
+            if(this.leds[s] == "yellow" || "green"){
+                this.ports[s].charge(min);
+            }
+            if(this.ports[s].juice >= .99){
+                this.leds[s] = "green";
+            }
+        }
     };
-
-
 }
 
-//defines the testing code
+
 function main(){}
 
 //runs the main code
--- Device.js
+++ Device.js
@@ -3,48 +3,65 @@
 function Device(t,ma,c){
 
     //Instance Variables
+    this.state= "off";
     this.type = t;
-    this.state = "off";
-    this.millAmps = am
-    this.capacity = c;
-    this.juice = 1;
+    this.milliamps= ma;
+    this.capacity=c;
+    this.juice=1
     this.rate = [0.0015,0.0235,0.23];
 
     //Instance Functions
+    this.off = function(){
+        if(this.state == "active" || this.state == "idle" || this.juice <= 0)
+           this.state = "off";
+    };
+
     this.on = function(){
         if(this.state == "off" && this.juice >0){
            this.state = "idle";
-        }
-        else if (/*complete*/){
-           //complete from instructions
+       }
+    };
+
+    this.wake = function(){
+        if(this.state == "off" || this.state == "idle"){
+            this.state == "active"
         }
     };
+
+    this.use = function(min){
+        let time = min / 60;
+
+        if(this.state == "off"){
+            let juice = juice - this.rate[0];
+        }
+        else if(this.state=="idle"){
+            let juice = juice - this.rate[1];
+        }
+        else if(this.state == "active"){
+            let juice = juice - this.rate[2];
+        }
+    }
 
     this.charge = function(min){
         //adds more electricity to the device's juice depending on its state
+        let time = min / 60;
+        let charge = (this.millAmps / this.capacity);
         if(this.state == "off"){
-            let charge = (this.millAmps / this.capacity);
-            let output = 1 - this.rate[0.0015];
-            let time = min / 60;
-            this.juice = this.juice + charge*output*time;
+            let output = 1 - this.rate[0];
         }
-        else if(this.state == "idle"){
-            let charge = (this.millAmps / this.capacity);
-            let output = 1 - this.rate[0.0235];
-            let time = min / 60;
-            this.juice = this.juice + charge*output*time;
+        else if(this.state=="idle"){
+            let output = 1 - this.rate[1];
         }
         else if(this.state == "active"){
-            let charge = (this.millAmps / this.capacity);
-            let output = 1 - this.rate[0.23];
-            let time = min / 60;
-            this.juice = this.juice + charge*output*time;
+            let output = 1 - this.rate[2];
         }
-
+this.juice = this.juice + charge*output*time;
         //resets juice to 1 if it has exceeded 1
-        if(){}
-
+        if(this.juice > 1){
+            this.juice = 1
+        }
     };
+
 
 }//end of the device declaration
 
@@ -52,4 +69,3 @@ function Device(t,ma,c){
 function main(){}
 //runs the testing code.
 main();
-                                                      
