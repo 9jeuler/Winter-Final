087178725e8882d758ad26ad178d64782f1070a8
9jeuler <31480902+9jeuler@users.noreply.github.com>
2017-12-14 09:12:01 -0600

    Add files via upload

--- ChargingDock.js
+++ ChargingDock.js
@@ -1,20 +1,22 @@
+const Device = require("./Device.js")
+
 function ChargingDock(){
 
-    this.ports = [undefined, undefined, undefined,undefined,undefined,undefined,undefined,undefined] //finish from instructions
-    this.leds = ["red","red","red","red","red","red","red","red",]
+    this.ports = ["undefined", "undefined", "undefined","undefined","undefined","undefined","undefined","undefined"]; //finish from instructions
+    this.leds = ["red","red","red","red","red","red","red","red"];
 
     this.plug = function(dvc){
         for(let s = 0; s < this.ports.length; s++){
             if (this.leds[s] == "red"){
-                this.port[s] = dvc;
+                this.ports[s] = dvc;
                 this.leds[s] = "yellow";
-                break
+                break;
             }
         }
     };
 
     this.unplug = function(dvcIdx){
-        if(this.leds[dvcIdx]  == "yellow" || this.leds[dvcIdx] == green){
+        if(this.leds[dvcIdx]  == "yellow" || this.leds[dvcIdx] == "green"){
             let temp = this.ports[dvcIdx];
             this.ports[dvcIdx] = undefined
             this.leds[dvcIdx] = "red"
@@ -24,10 +26,10 @@ function ChargingDock(){
 
     this.chargeAll = function(min){
         for(let s = 0; s < this.ports.length; s++){
-            if(this.leds[s] == "yellow" || "green"){
+            if(this.leds[s] == "yellow" || this.leds[s] == "green"){
                 this.ports[s].charge(min);
             }
-            if(this.ports[s].juice >= .99){
+            if(this.ports[s].juice >= 0.99){
                 this.leds[s] = "green";
             }
         }
@@ -35,7 +37,30 @@ function ChargingDock(){
 }
 
 
-function main(){}
+function main(){
+    let cd = new ChargingDock();
+
+    let d1 = new Device("phone",3000,10000);
+    let d2 = new Device("laptop",3000,15000);
+    let d3 = new Device("laptop",5000,15000);
+    let d4 = new Device("tablet",3000,15000);
+
+    d1.use(90);
+    d2.use(120);
+    d3.use(90);
+    d4.use(240);
+    console.log(cd);
+    cd.plug(d1);
+    cd.plug(d2);
+    cd.plug(d3);
+    cd.plug(d4);
+    console.log(cd);
+    cd.chargeAll(60);
+
+    cd.unplug(0);
+    cd.unplug(1);
+    console.log(cd);
+}
 
 //runs the main code
 main();
--- Device.js
+++ Device.js
@@ -1,5 +1,3 @@
-//Defines how all Devices with work.
-
 function Device(t,ma,c){
 
     //Instance Variables
@@ -11,20 +9,28 @@ function Device(t,ma,c){
     this.rate = [0.0015,0.0235,0.23];
 
     //Instance Functions
+    this.power = function(){
+        return this.juice
+    };
+
     this.off = function(){
         if(this.state == "active" || this.state == "idle" || this.juice <= 0)
-           this.state = "off";
+        this.state = "off";
     };
 
     this.on = function(){
         if(this.state == "off" && this.juice >0){
-           this.state = "idle";
-       }
+            this.state = "idle";
+        }
     };
-
+    this.sleep = function(){
+        if(this.state == "active" && this.juice > 0){
+            this.state = "idle"
+        }
+    }
     this.wake = function(){
         if(this.state == "off" || this.state == "idle"){
-            this.state == "active"
+            this.state = "active"
         }
     };
 
@@ -32,30 +38,31 @@ function Device(t,ma,c){
         let time = min / 60;
 
         if(this.state == "off"){
-            let juice = juice - this.rate[0];
+             this.juice = this.juice - this.rate[0];
         }
         else if(this.state=="idle"){
-            let juice = juice - this.rate[1];
+             this.juice = this.juice - this.rate[1];
         }
         else if(this.state == "active"){
-            let juice = juice - this.rate[2];
+             this.juice = this.juice - this.rate[2];
         }
     }
 
     this.charge = function(min){
         //adds more electricity to the device's juice depending on its state
+        let output
         let time = min / 60;
-        let charge = (this.millAmps / this.capacity);
+        let charge = (this.milliamps / this.capacity);
         if(this.state == "off"){
-            let output = 1 - this.rate[0];
+            output = 1 - this.rate[0];
         }
         else if(this.state=="idle"){
-            let output = 1 - this.rate[1];
+            output = 1 - this.rate[1];
         }
         else if(this.state == "active"){
-            let output = 1 - this.rate[2];
+            output = 1 - this.rate[2];
         }
-this.juice = this.juice + charge*output*time;
+        this.juice = this.juice + charge*output*time;
         //resets juice to 1 if it has exceeded 1
         if(this.juice > 1){
             this.juice = 1
@@ -64,8 +71,5 @@ this.juice = this.juice + charge*output*time;
 
 
 }//end of the device declaration
-
+module.exports= Device;
 //defines the testing code.
-function main(){}
-//runs the testing code.
-main();
